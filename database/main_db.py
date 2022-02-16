61f0df708946553b21e27dd1c14969d0788b58d8
jobert <EpicLegitPlayer@gmail.com>
2022-02-16 23:22:22 +0800

    added methods for mysqldb

---
+++ database/main_db.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+class mysqldb:
+    def __init__(self):
+        connection = None
+        session = None
+    
+    def open_connection(self):
+        db = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='password',
+            database='testdb'
+        )
+
+        self.connection = db
+        self.session = db.cursor()
+    
+    def close_connection(self):
+        self.session.close()
+        self.connection.close()
