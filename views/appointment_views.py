835657e74eefd192886a1d463050441900b0f5c9
jobert <EpicLegitPlayer@gmail.com>
2022-02-26 14:25:02 +0800

    added notification functionality, incomplete

--- views/appointment_views.py
+++ views/appointment_views.py
@@ -1,16 +1,26 @@
-from flask import render_template, session
+from flask import render_template, session, url_for, redirect
 from flask.views import MethodView
 from flask_login import login_required
 from models.models import user_information
 import folium
 from geopy.geocoders import Nominatim
 import openrouteservice as ors
+import os
+from twilio.rest import Client
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 class ScheduleAppointmentView(MethodView):
     decorators = [login_required]
     def get(self):
         self._render_map()
         return render_template("ScheduleAppointment.html")
+    
+    def post(self):
+        self._send_sms()
+        self._send_email()
+        return redirect(url_for("ViewAppointment"))
+
 
     def _render_map(self):
         # REMINDER!!!
@@ -45,6 +55,47 @@ class ScheduleAppointmentView(MethodView):
         ).add_to(folium_map)
 
         folium_map.save('static/map.html')
+    
+    def _send_sms(self):
+        current_user = user_information.query.filter_by(email_address=session["user"]).first()
+        user_contact = current_user.contact_number
+        
+        account_sid = os.environ["ACCOUNT_SID"]
+        auth_token = os.environ["AUTH_TOKEN"]
+
+        message = f"""Hello! Here are the details of your appointment:
+        Hospital Name: 
+        Hospital Address: 
+        Available Vaccines: 
+        Expected Date of Vaccination: """
+
+        twilio_client = Client(account_sid, auth_token)
+        twilio_client.messages.create(
+            to = user_contact,
+            from_= "+19035609492",
+            body = message
+        )
+
+
+    def _send_email(self):
+        current_user = user_information.query.filter_by(email_address=session["user"]).first()
+        user_email = current_user.email_address
+        from_sender = os.environ["FROM_SENDER"]
+        sendgrid_api = os.environ["SENDGRID_API"]
+
+        content = f"""Hello! Here are the details of your appointment:
+        Hospital Name: 
+        Hospital Address: 
+        Available Vaccines: 
+        Expected Date of Vaccination: """
+        
+        mail_message = Mail(from_email=from_sender,
+               to_emails=user_email,
+               subject="Your Vaccine Appointment Summary",
+               plain_text_content="This is a test content2")
+        
+        sg = SendGridAPIClient(sendgrid_api)
+        response = sg.send(mail_message)
 
 class ViewAppointmentView(MethodView):
     decorators = [login_required]
