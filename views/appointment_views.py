583e1e4dfeecc31f0c8b68270c210d8c540d87c3
jobert <EpicLegitPlayer@gmail.com>
2022-02-28 12:30:42 +0800

    fixed appointment functions

--- views/appointment_views.py
+++ views/appointment_views.py
@@ -8,6 +8,11 @@ import folium
 from geopy.geocoders import Nominatim
 import openrouteservice as ors
 import json
+from sqlalchemy import inspect
+import os
+from twilio.rest import Client
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 from math import radians, cos, sin, asin, sqrt
 from datetime import date, datetime
@@ -37,7 +42,6 @@ class ScheduleAppointmentView(MethodView):
 
     def get(self):
         low = self._render_map()
-        print(low)
         hospital_data = self._get_hospital_data()
         json_data = json.dumps(hospital_data)
         return render_template("ScheduleAppointment.html", lowestdistance=low, form=self.form(), hospital_data=hospital_data, json_data=json_data)
@@ -184,19 +188,65 @@ class ScheduleAppointmentView(MethodView):
                                                         avail_dates]
 
         return hospital_data
+    
+
+    def _send_sms(self, form_data):
+        current_user = user_information.query.filter_by(email_address=session["user"]).first()
+        user_contact = current_user.contact_number
+        
+        account_sid = os.environ["ACCOUNT_SID"]
+        print(account_sid)
+        auth_token = os.environ["AUTH_TOKEN"]
+        print(auth_token)
+        message = f"""Hello! Here are the details of your appointment:
+        Hospital Name: {form_data.hospitalname.data}
+        Hospital Address: {form_data.hospitaladdress.data}
+        Available Vaccines: {form_data.availablevaccines.data}
+        Expected Date of Vaccination: {form_data.vaccineschedule.data}"""
+        twilio_client = Client(account_sid, auth_token)
+        twilio_client.messages.create(
+            to = user_contact,
+            from_= "+19035609492",
+            body = message
+        )
+
+    def _send_email(self, form_data):
+        current_user = user_information.query.filter_by(email_address=session["user"]).first()
+        user_email = current_user.email_address
+        from_sender = os.environ["FROM_SENDER"]
+        sendgrid_api = os.environ["SENDGRID_API"]
+        content = f"""Hello! Here are the details of your appointment:
+        Hospital Name: {form_data.hospitalname.data}
+        Hospital Address: {form_data.hospitaladdress.data}
+        Available Vaccines: {form_data.availablevaccines.data}
+        Expected Date of Vaccination: {form_data.vaccineschedule.data}"""
+        
+        mail_message = Mail(from_email=from_sender,
+               to_emails=user_email,
+               subject="Your Vaccine Appointment Summary",
+               plain_text_content=content)
+        
+        sg = SendGridAPIClient(sendgrid_api)
+        response = sg.send(mail_message)
 
 
     def post(self):
         form = self.form()
         if form.validate_on_submit():
-            user = availability_details(hosp_name=form.hospitalname.data, hosp_address=form.hospitaladdress.data,
-                                        vaccine_name=form.availablevaccines.data, availability_date=form.vaccineschedule.data)
+            availability_date=form.vaccineschedule.data
 
-            db.session.add(user)
+            availability = availability_details.query.filter_by(availability_date=availability_date).first()
+            user = user_information.query.filter_by(email_address=session["user"]).first()
+            
+            user.schedule = availability.id
             db.session.commit()
-            session["vacid"] = user.vaccine_id
-            session['hospid'] = user.hosp_id
-            session["schedid"] = 0
+
+            self._send_sms(form)
+            self._send_email(form)
+
+            #session["vacid"] = user.vaccine_id
+            #session['hospid'] = user.hosp_id
+            #session["schedid"] = 0
             flash(f'You have made an appointment' , category = 'success')
             return redirect(url_for('ViewAppointment'))
 
@@ -208,13 +258,12 @@ class ViewAppointmentView(MethodView):
 
     def get(self):
 
-       view = vaccine.query\
-        .join(hospital, hospital.hosp_id == vaccine.vaccine_id) \
-        .add_columns(vaccine.vaccine_id, vaccine.vaccine_name,vaccine.hos, hospital.hosp_id,hospital.hosp_name,hospital.hosp_id) \
-        .join(availability_details, availability_details.vac == vaccine.vaccine_id)\
-        .add_columns(appointment.id, appointment.user_id,appointment.availability_id, availability_details.hos,availability_details.availability_date,availability_details.availability_time1,availability_details.vac) \
-        .join(appointment, appointment.availability_id == availability_details.id) \
-        .add_columns(user_information.user_id,user_information.first_name,user_information.middle_name,user_information.last_name,user_information.email_address) \
-        .filter(user_information.email_address == session["user"]).first()
+        view = db.session.query(user_information, availability_details, hospital, vaccine)\
+            .select_from(user_information)\
+            .join(availability_details, availability_details.id == user_information.schedule)\
+            .join(hospital, hospital.hosp_id == availability_details.hos)\
+            .join(vaccine, vaccine.vaccine_id == availability_details.vac)\
+            .first()
 
-       return render_template("ViewAppointment.html", view=view)
+        print(view.user_information.first_name)
+        return render_template("ViewAppointment.html", view=view)
